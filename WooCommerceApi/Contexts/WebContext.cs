636a17fa4963860032512efc8c4398cc8fe19711
Erfan4708 <abbasy.4708@gmail.com>
2024-11-13 20:40:07 +0330

    Refactor and implement methods with new approach

--- WooCommerceApi/Contexts/WebContext.cs
+++ WooCommerceApi/Contexts/WebContext.cs
@@ -1,83 +1,338 @@
-﻿using RestSharp;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
 using WebApi.Contexts;
+using WebApi.Contexts.Interfaces;
+using WebApi.Exceptions;
 using WebApi.Models;
-using WebApi.Repositories.Entities;
-using WooCommerceApi.Repositories.Entities;
-using WooCommerceApi.Repositories.WooRepositories;
-using WooCommerceApi.Repositories.WooRepositories.Interfaces;
+using WooCommerceApi.Helpers;
+using WooCommerceApi.Models.WooCommerceModels;
+using Exception = System.Exception;
 
 namespace WooCommerceApi.Contexts
 {
-    public class WebContext : IWebContext
+    public class WebContext : BaseWebContext, IWebContext
     {
-        private readonly Dictionary<string, string> _apiConfigs;
-        protected IRestClient _httpClient;
-        private IProductRepository _webProductRepository;
-        private IOrderRepository _webOrderRepository;
-        private ICategoryRepository _webCategoryRepository;
+        private readonly RestClient  _client;
+        private int _currentPage = 1;
+        private int _pageSize = 10;
+        private int _maxPage = int.MaxValue;
 
-        public WebContext(Dictionary<string, string> apiConfigs)
+        public WebContext(Dictionary<string, string> configs) : base(configs)
         {
-            _apiConfigs = apiConfigs;
-        }
-
-        public IProductRepository WebProductRepository
-        {
-            get
+            try
             {
-                if (_webProductRepository  == null)
+                var url = configs["WooCommerceUrl"];
+                var consumerKey = configs["WooCommerceConsumerKey"];
+                var consumerSecret = configs["WooCommerceConsumerSecret"];
+                const string path = "wp-json/wc/v3";
+                _client = new RestClient(new Uri(new Uri(url), path));
+                
+                // Set default headers
+                _client.AddDefaultHeader("Accept", "application/json");
+
+                // Set authentication using WooCommerce API keys
+                var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{consumerKey}:{consumerSecret}"));
+                _client.AddDefaultHeader("Authorization", $"Basic {credentials}");
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidFieldException(ex.Source, ex.Message);
+            }
+        }
+        private Task<T> SendRequest<T>(RestRequest request, object body = null)
+        {
+            try
+            {
+                // If there is a body, serialize it to JSON and add it to the request
+                if (body != null)
                 {
-                    IWooProductRepository wooProduct = new WooProductRepository(_apiConfigs);
-                    _webProductRepository = new WebProductRepository(_apiConfigs, "products", wooProduct);
+                    var jsonBody = JsonConvert.SerializeObject(body);
+                    request.AddJsonBody(jsonBody);
                 }
-                return _webProductRepository;
+                var response = _client.Execute(request);
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.OK:
+                    case HttpStatusCode.Created:  // Handle created status for POST requests
+                        return Task.FromResult(JsonConvert.DeserializeObject<T>(response.Content ?? string.Empty));
+                    case HttpStatusCode.NotFound:
+                        throw new DoesNotExistException();
+                    case HttpStatusCode.BadRequest:
+                        throw new InvalidFieldException("BadRequest", response.Content);
+                    case HttpStatusCode.Unauthorized:
+                        throw new InvalidFieldException("Unauthorized", response.Content);
+                    default:
+                        throw new InvalidFieldException($"Error! Status code: {response.StatusCode}", response.Content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"HTTP Request error: {ex.Message}");
+                throw;
             }
         }
 
-        public IOrderRepository WebOrderRepository
+        /// <summary>
+        /// Retrieves all entities using a custom processing function for each page.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="processPage">A function to process each page of results. It should return true if you want to continue fetching more pages, false to stop.</param>
+        /// <returns>Enumerable of entities of type <typeparamref name="T"/>.</returns>
+        private IEnumerable<T> GetAllWithPagination<T>(RestRequest request, Func<IEnumerable<T>, bool> processPage)
         {
-            get
+            var results = new List<T>();
+            while (true)
             {
-                if (_webOrderRepository == null)
+                // Deserialize the response content into the expected entity type
+                var result = SendRequest<IList<T>>(request).Result;
+                if (result == null || !result.Any())
                 {
-                    IWooOrderRepository wooOrder = new WooOrderRepository(_apiConfigs);
-                    _webOrderRepository = new WebOrderRepository(wooOrder);
+                    // No more results, stop pagination
+                    break;
                 }
-                return _webOrderRepository;
+
+                // Add the results from this page to the overall collection
+                results.AddRange(result);
+
+                // Call the processing function for the current page
+                // If the function returns false, stop fetching more pages
+                if (!processPage(result) || _currentPage >= _maxPage)
+                {
+                    break;
+                }
+
+                // Move to the next page
+                NextPage(request);
             }
+            ResetPage(request);
+            return results;
         }
 
-        public ICategoryRepository WebCategoryRepository
+        private void ResetPage(RestRequest request)
         {
-            get
+            _currentPage = 1;
+            request.AddOrUpdateParameter("size", _pageSize.ToString());
+            request.AddOrUpdateParameter("page", _currentPage.ToString());
+        }
+
+        /// <summary>
+        /// Advances to the next page in the paginated results.
+        /// </summary>
+        private void NextPage(RestRequest request)
+        {
+            _currentPage++;
+            request.AddOrUpdateParameter("size", _pageSize.ToString());
+            request.AddOrUpdateParameter("page", _currentPage.ToString());
+        }
+
+        /// <summary>
+        /// Sets the endpoint with pagination and any additional filters using the QueryFilterBuilder.
+        /// </summary>
+        /// <param name="entity"></param>
+        //private void SetParameters(string endpoint)
+        //{
+            //_queryBuilder
+             //   .AddOr
+            //_endpoint = $"wp-json/wc/v3/{endpoint}";
+        //}UpdateParameter("page", _currentPage.ToString())
+             //   .AddOrUpdateParameter("per_page", "100");
+
+        public new int CreateProduct(WebProduct entity)
+        {
+            const string endpoint = "products";
+            var wooProduct = WooCommerceConverters.ToWooProduct(entity);
+            var request = new RestRequest(endpoint, Method.Post);
+            var createdProduct = SendRequest<WooProduct>(request, wooProduct).Result;
+            return WooCommerceConverters.TryToInt(createdProduct.Id);
+        }
+        public async Task<int> UpdateProductAsync(int id, WebProduct entity)
+        {
+            var existingProduct = GetProductById(id);
+            if (existingProduct == null)
             {
-                if (_webCategoryRepository == null)
-                {
-                    IWooCategoryRepository wooCategory = new WooCategoryRepository(_apiConfigs);
-                    _webCategoryRepository = new WebCategoryRepository(wooCategory);
-                }
-                return _webCategoryRepository;
+                throw new DoesNotExistException();
             }
+            var endpoint = $"products/{id}";
+            var request = new RestRequest(endpoint, Method.Put);
+            var updatedProductData = WooCommerceConverters.ToWooProduct(entity);
+            var updatedProduct = await SendRequest<WooProduct>(request, updatedProductData);
+
+            return WooCommerceConverters.TryToInt(updatedProduct.Id);
         }
 
-        #region IDisposable Implementation
-        protected virtual void Dispose(bool disposing) { }
-
-        public void Dispose()
+        public new WebProduct GetProductById(int id)
         {
-            Dispose(true);
-            GC.Collect();
+            var endpoint = $"products/{id}";
+            var request = new RestRequest(endpoint, Method.Get);
+            var wooProduct = SendRequest<WooProduct>(request).Result;
+            return WooCommerceConverters.ToWebProduct(wooProduct);
         }
 
-        ~WebContext()
+        public new IEnumerable<WebProduct> GetAllProducts()
         {
-            Dispose(false);
+            const string endpoint = "products";
+            var request = new RestRequest(endpoint, Method.Get);    
+            var results = new List<WooProduct>();
+            return GetAllWithPagination<WooProduct>(request, pageResults =>
+            {
+                results.AddRange(pageResults);
+                return true;
+            }).Select(WooCommerceConverters.ToWebProduct).ToList();
         }
-        #endregion
+
+        public new int GetTotalProductsCount(string searchTerm)
+        {
+            const string endpoint = "products";
+            var request = new RestRequest(endpoint, Method.Get);
+            _maxPage = int.MaxValue;
+            _pageSize = 100;
+            ResetPage(request);
+            if (searchTerm != null)
+            {
+                request.AddParameter("search", searchTerm);
+            }
+            var results = new List<WooProduct>();
+            return GetAllWithPagination<WooProduct>(request, pageResults =>
+            {
+                results.AddRange(pageResults);
+                return true;
+            }).Count();
+        }
+
+        public new IEnumerable<WebProduct> GetAllProductsExcludingIds(IList<int> idsToExclude)
+        {
+            const string endPoint = "products";
+            var idsToExcludeString = string.Join(",", idsToExclude);
+            var request = new RestRequest(endPoint, Method.Get);
+            request.AddParameter("exclude", idsToExcludeString);
+            var products = SendRequest<IList<WooProduct>>(request).Result;
+            return products.Select(WooCommerceConverters.ToWebProduct)
+                .Where(p => !idsToExclude.Contains(p.Id)).ToList(); 
+        }
+        public new IEnumerable<WebProduct> SearchProducts(string searchTerm, int page = 1, int pageSize = 10, int maxPage = 1)
+        {
+            _currentPage = page < 1 ? 1 : page;
+            _pageSize = pageSize > 100 || pageSize < 1 ? 100 : pageSize;
+            _maxPage = maxPage < 1 ? 1 : maxPage;
+            const string endPoint = "products";
+            var request = new RestRequest(endPoint, Method.Get);
+            if (searchTerm == null)
+            {
+                return new List<WebProduct>();
+            }
+            
+            request.AddParameter("search", searchTerm);
+            var results = new List<WooProduct>();
+            return GetAllWithPagination<WooProduct>(request, pageResults =>
+            {
+                results.AddRange(pageResults);
+                return true;
+            }).Select(WooCommerceConverters.ToWebProduct).ToList();
+        }
+        public new IEnumerable<WebProduct> GetAllProductsWithFields(IList<string> fields)
+        {
+            if (fields == null || !fields.Any())
+            {
+                return new List<WebProduct>();
+            }
+            const string endPoint = "products";
+            var fieldsString = string.Join(",", fields);
+            var request = new RestRequest(endPoint, Method.Get);
+            request.AddParameter("_fields", fieldsString);
+            var results = new List<WooProduct>();
+            return GetAllWithPagination<WooProduct>(request, pageResults =>
+            {
+                results.AddRange(pageResults);
+                return true;
+            }).Select(WooCommerceConverters.ToWebProduct).ToList();
+        }
+
+        public new int GetMaxProductId()
+        {
+            const string endpoint = "products";
+            var request = new RestRequest(endpoint, Method.Get);
+            var products = SendRequest<IList<WooProduct>>(request).Result;
+            return products.Max(product => WooCommerceConverters.TryToInt(product.Id));
+        }
+
+        public new WebCategory GetCategoryById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public new IEnumerable<WebCategory> GetAllCategories()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public new IList<WebCategory> GetAllCategoriesExcludingIds(IEnumerable<int> idsToExclude)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public new int CreateCategory(WebCategory entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public new int UpdateCategory(int id, WebCategory entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public new IList<WebCategory> GetAllCategoriesWithFields(IList<string> fields)
+        {
+            if (fields == null || !fields.Any())
+            {
+                return new List<WebCategory>();
+            }
+            const string endPoint = "products/categories";
+            var fieldsString = string.Join(",", fields);
+            var request = new RestRequest(endPoint, Method.Get);
+            request.AddParameter("_fields", fieldsString);
+            var results = new List<WooCategory>();
+            return GetAllWithPagination<WooCategory>(request, pageResults =>
+            {
+                results.AddRange(pageResults);
+                return true;
+            }).Select(WooCommerceConverters.ToWebCategory).ToList();
+        }
+
+        public new int GetMaxCategoryId()
+        {
+            throw new System.NotImplementedException();
+        }
+        public new IList<WebCategory> SearchCategories(string searchTerm, int page = 1, int pageSize = 10, int maxPage = 1)
+        {
+            _currentPage = page < 1 ? 1 : page;
+            _pageSize = pageSize > 100 || pageSize < 1 ? 100 : pageSize;
+            _maxPage = maxPage < 1 ? 1 : maxPage;
+            const string endPoint = "products/categories";
+            var request = new RestRequest(endPoint, Method.Get);
+            if (searchTerm == null)
+            {
+                return new List<WebCategory>();
+            }
+            
+            request.AddParameter("search", searchTerm);
+            var results = new List<WooCategory>();
+            return GetAllWithPagination<WooCategory>(request, pageResults =>
+            {
+                results.AddRange(pageResults);
+                return true;
+            }).Select(WooCommerceConverters.ToWebCategory).ToList();
+        }
+
+        public new void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
