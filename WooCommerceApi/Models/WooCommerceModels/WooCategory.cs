fdff20a0231ad7384ddb29bb81bbda028cde73b6
Erfan4708 <abbasy.4708@gmail.com>
2024-11-17 00:46:05 +0330

    Add JsonProperty for each property of WooCategory model

--- WooCommerceApi/Models/WooCommerceModels/WooCategory.cs
+++ WooCommerceApi/Models/WooCommerceModels/WooCategory.cs
@@ -1,46 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WooCommerceApi.Models.WooCommerceModels
 {
-    public class WooCategory
+    public class WooCategory : MinimalWooCategory
     {
-        public int Id { get; set; }
 
+        [JsonProperty("name")]
         public string Name { get; set; }
 
+        [JsonProperty("slug")]
         public string Slug { get; set; }
 
+        [JsonProperty("parent")]
         public int? Parent { get; set; }
 
+        [JsonProperty("description")]
         public string Description { get; set; }
         
+        [JsonProperty("count")]
         public int Count { get; set; }
     }
 
     public class WooCategoryImage
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
 
+        [JsonProperty("src")]
         public string Src { get; set; }
 
+        [JsonProperty("name")]
         public string Name { get; set; }
 
+        [JsonProperty("alt")]
         public string Alt { get; set; }
     }
 
     public class WooCategoryLinks
     {
+        [JsonProperty("self")]
         public WooCategoryLink[] Self { get; set; }
 
+        [JsonProperty("collection")]
         public WooCategoryLink[] Collection { get; set; }
     }
 
     public class WooCategoryLink
     {
+        [JsonProperty("href")]
         public string Href { get; set; }
     }
 }
