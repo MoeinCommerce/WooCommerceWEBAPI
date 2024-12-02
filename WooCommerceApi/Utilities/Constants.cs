406f58db3ab0f5ec50cef706ad29f640ef8c3625
Erfan4708 <abbasy.4708@gmail.com>
2024-12-02 17:29:41 +0330

    Store constants payment methods in a dictionary

--- WooCommerceApi/Utilities/Constants.cs
+++ WooCommerceApi/Utilities/Constants.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace WooCommerceApi.Utilities
 {
@@ -10,5 +11,16 @@ namespace WooCommerceApi.Utilities
     {
         public static string CONSUMER_KEY = "ck_0b65361a66a4784f7fa6c1f7078e50f9e6521516";
         public static string CONSUMER_SECRET = "cs_8e1d1d6c0439c5a0d0452c886b1485b5bb505280";
+        public static Dictionary<string, int> PaymentMethods = new Dictionary<string, int>
+        {
+            {"cod", 1},
+            {"bacs", 2},
+            {"cheque", 3},
+            {"paypal", 4},
+            {"stripe", 5},
+            {"bankmellat", 6},
+            {"WC_ZPal", 7},
+            {"WC_IDPay", 8}
+        };
     }
 }
