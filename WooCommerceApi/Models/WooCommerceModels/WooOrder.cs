52900b285c2061de71d5c5e216182a307d17246b
Erfan4708 <abbasy.4708@gmail.com>
2024-12-02 17:31:08 +0330

    Add json property into WooOrder model

--- WooCommerceApi/Models/WooCommerceModels/WooOrder.cs
+++ WooCommerceApi/Models/WooCommerceModels/WooOrder.cs
@@ -3,39 +3,103 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WooCommerceApi.Models.WooCommerceModels
 {
     public class WooOrder
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
+
+        [JsonProperty("customer_id")]
+        public int CustomerId { get; set; }
+        
+        [JsonProperty("transaction_id")]
+        public string TransactionId { get; set; }
+        
+        [JsonProperty("payment_method")]
+        public string PaymentMethod { get; set; }
+        
+        [JsonProperty("status")]
         public string Status { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime DateModified { get; set; }
+        
+        [JsonProperty("date_created")]
+        public DateTime? DateCreated { get; set; }
+        
+        [JsonProperty("date_modified")]
+        public DateTime? DateModified { get; set; }
+        
+        [JsonProperty("currency")]
         public string Currency { get; set; }
-        public string Total { get; set; }
-        public string TotalTax { get; set; }
+        
+        [JsonProperty("shipping_total")]
+        public double ShippingTotal { get; set; }
+        
+        [JsonProperty("total")]
+        public double Total { get; set; }
+        
+        [JsonProperty("total_tax")]
+        public double TotalTax { get; set; }
+        
+        [JsonProperty("billing")]
         public WooCustomer Billing { get; set; }
+        
+        [JsonProperty("shipping")]
         public WooCustomer Shipping { get; set; }
+        
+        [JsonProperty("line_items")]
         public List<WooOrderLineItem> LineItems { get; set; }
+        
+        [JsonProperty("shipping_lines")]
         public List<WooOrderShippingLine> ShippingLines { get; set; }
+        
+        [JsonProperty("fee_lines")]
         public List<WooOrderFeeLine> FeeLines { get; set; }
+        
+        [JsonProperty("coupon_lines")]
         public List<WooOrderCouponLine> CouponLines { get; set; }
+        
+        [JsonProperty("tax_lines")] 
         public List<WooOrderTaxLine> TaxLines { get; set; }
     }
 
     public class WooOrderLineItem
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
+        
+        [JsonProperty("name")]
         public string Name { get; set; }
+        
+        [JsonProperty("product_id")]
         public int ProductId { get; set; }
+        
+        [JsonProperty("variation_id")]
         public int VariationId { get; set; }
+        
+        [JsonProperty("quantity")]
         public int Quantity { get; set; }
+        
+        [JsonProperty("tax_class")]
         public string TaxClass { get; set; }
-        public string Subtotal { get; set; }
-        public string SubtotalTax { get; set; }
-        public string Total { get; set; }
-        public string TotalTax { get; set; }
+        
+        [JsonProperty("subtotal")]
+        public double Subtotal { get; set; }
+        
+        [JsonProperty("subtotal_tax")]
+        public double SubtotalTax { get; set; }
+        
+        [JsonProperty("total")]
+        public double Total { get; set; }
+        
+        [JsonProperty("total_tax")]
+        public double TotalTax { get; set; }
+        
+        [JsonProperty("price")]
+        public double Price { get; set; }
+        
+        [JsonProperty("taxes")]
         public List<WooOrderTax> Taxes { get; set; }
     }
 
