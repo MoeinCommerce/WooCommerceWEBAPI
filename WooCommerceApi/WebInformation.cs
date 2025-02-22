490a0913dd33768bc5cea04e5ea59bf25378c2a9
Erfan4708 <abbasy.4708@gmail.com>
2025-02-22 10:50:54 +0330

    Bump version to 1.0.3

--- WooCommerceApi/WebInformation.cs
+++ WooCommerceApi/WebInformation.cs
@@ -18,7 +18,7 @@ namespace WooCommerceApi
 
         public string ExecutablePath => AppDomain.CurrentDomain.BaseDirectory;
 
-        public string Version => "1.0.2";
+        public string Version => "1.0.3";
         public string IconPath => string.Empty;
 
         public List<WebConfig> Configurations =>
