c02236dada73490496851dbcaa2fd9a0d131378a
Erfan4708 <abbasy.4708@gmail.com>
2025-04-14 15:42:37 +0330

    chore: bump version to 1.2.9 in WebInformation class

--- WooCommerceApi/WebInformation.cs
+++ WooCommerceApi/WebInformation.cs
@@ -18,7 +18,7 @@ namespace WooCommerceApi
 
         public string ExecutablePath => AppDomain.CurrentDomain.BaseDirectory;
 
-        public string Version => "1.2.8";
+        public string Version => "1.2.9";
         public string IconPath => string.Empty;
 
         public List<WebConfig> Configurations =>
